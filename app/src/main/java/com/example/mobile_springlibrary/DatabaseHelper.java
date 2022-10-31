946256bdcf09ef2f57d25dcd912493557b6f4004
LarissaSonoda <sonoda.lari@gmail.com>
2022-10-31 10:30:14 -0300

    banco criado

--- app/src/main/java/com/example/mobile_springlibrary/DatabaseHelper.java
+++ app/src/main/java/com/example/mobile_springlibrary/DatabaseHelper.java
@@ -16,18 +16,39 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
         @Override
         public void onCreate(SQLiteDatabase db){
-            db.execSQL("CREATE TABLE tbUser(" +
+            db.execSQL("CREATE TABLE Cliente(" +
                     "UserCode INTEGER PRIMARY KEY AUTOINCREMENT," +
                     "UserName TEXT NOT NULL," +
                     "UserPassword TEXT NOT NULL,"+
                     "UserImage BYTE)");
+            db.execSQL("CREATE TABLE Livro(" +
+                    "ISBNLivro INTEGER PRIMARY KEY," +
+                    "titLivro TEXT NOT NULL," +
+                    "titOriLiv TEXT NOT NULL," +
+                    "sinopLiv TEXT NOT NULL," +
+                    "imgLiv BYTE," +
+                    "precoLiv FLOAT NOT NULL," +
+                    "numPagLiv INTEGER NOT NULL," +
+                    "anoLiv INTEGER NOT NULL," +
+                    "numEdicaoLiv INTEGER NOT NULL)");
+            db.execSQL("CREATE TABLE Carrinho(" +
+                    "IdCarrinho INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    "valTotal FLOAT NOT NULL)");
+            db.execSQL("CREATE TABLE ItemCarrinho(" +
+                    "IdItem INTEGER PRIMARY KEY," +
+                    "nomItem TEXT NOT NULL," +
+                    "PrecoItem FLOAT NOT NULL," +
+                    "qtdItem INTEGER NOT NULL," +
+                    "IdProd INTEGER NOT NULL," +
+                    "FOREIGN KEY (IdProd) REFERENCES Livro(ISBNLivro))");
         }
 
         @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
-            db.execSQL(
-                    "drop table if exists tbUser"
-            );
+            db.execSQL( "drop table if exists Cliente");
+            db.execSQL( "drop table if exists Livro");
+            db.execSQL( "drop table if exists Carrinho");
+            db.execSQL( "drop table if exists ItemCarrinho");
             onCreate(db);
         }
 
