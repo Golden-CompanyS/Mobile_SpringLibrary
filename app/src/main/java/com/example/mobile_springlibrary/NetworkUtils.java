69d691fdc27af264ded0ebf6d552ecc8cf459fba
Larissa  Miranda Sonoda <sonoda.lari@gmail.com>
2022-11-08 21:19:52 -0300

    alterações na conexão com a api

--- app/src/main/java/com/example/mobile_springlibrary/NetworkUtils.java
+++ app/src/main/java/com/example/mobile_springlibrary/NetworkUtils.java
@@ -1,4 +1,63 @@
 package com.example.mobile_springlibrary;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.net.HttpURLConnection;
+
 public class NetworkUtils {
+    private static final String URL_API = "";
+    private static HttpURLConnection connec;
+    private static BufferedReader reader;
+    private static String springLib;
+    static String url = null;
+
+    static String searchBook(String queryString){
+        String BookJSONString = null;
+
+        try{
+            BookJSONString = searchBook(queryString);
+            if(BookJSONString == null){
+                return null;
+            }
+
+            JSONObject bookJSONObject = new JSONObject(BookJSONString);
+
+            String ISBN = null;
+            String titLiv = null;
+            String titOriLiv = null;
+            String sinopLiv = null;
+            String imgLiv = null;
+            String numPagLiv = null;
+            String anoLiv = null;
+            String numEdicaoLiv = null;
+
+            try{
+                ISBN = makeupJSONObject.getString("isbn");
+                priceProduct = makeupJSONObject.getString("price");
+                descriptionProduct = makeupJSONObject.getString("description");
+                brandProduct = makeupJSONObject.getString("brand");
+                typeProduct = makeupJSONObject.getString("product_type");
+            } catch (JSONException e){
+                e.printStackTrace();
+            }
+
+            JSONObject productJSONObject = new JSONObject();
+            try{
+                productJSONObject.put("name", nameProduct);
+                productJSONObject.put("price", priceProduct);
+                productJSONObject.put("description", descriptionProduct);
+                productJSONObject.put("brand", brandProduct);
+                productJSONObject.put("product_type", typeProduct);
+
+                makeupJSONString = makeupJSONObject.toString();
+            } catch (JSONException e){
+                e.printStackTrace();
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Log.d(LOG_TAG, makeupJSONString);
+        return makeupJSONString;
+    }
 }
