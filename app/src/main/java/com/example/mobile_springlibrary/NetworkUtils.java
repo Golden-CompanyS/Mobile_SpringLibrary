f8a0a6397012d8bbb9e0ee4385ea664496a8f435
LarissaSonoda <sonoda.lari@gmail.com>
2022-11-10 12:12:21 -0300

    Alterações na network utils

--- app/src/main/java/com/example/mobile_springlibrary/NetworkUtils.java
+++ app/src/main/java/com/example/mobile_springlibrary/NetworkUtils.java
@@ -1,5 +1,10 @@
 package com.example.mobile_springlibrary;
 
+import static androidx.core.content.PackageManagerCompat.LOG_TAG;
+
+import android.util.Log;
+
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -10,6 +15,7 @@ public class NetworkUtils {
     private static HttpURLConnection connec;
     private static BufferedReader reader;
     private static String springLib;
+    private static final String LOG_TAG = NetworkUtils.class.getSimpleName();
     static String url = null;
 
     static String searchBook(String queryString){
@@ -33,31 +39,90 @@ public class NetworkUtils {
             String numEdicaoLiv = null;
 
             try{
-                ISBN = makeupJSONObject.getString("isbn");
-                priceProduct = makeupJSONObject.getString("price");
-                descriptionProduct = makeupJSONObject.getString("description");
-                brandProduct = makeupJSONObject.getString("brand");
-                typeProduct = makeupJSONObject.getString("product_type");
+                ISBN = bookJSONObject.getString("isbn");
+                titLiv = bookJSONObject.getString("titLiv");
+                titOriLiv = bookJSONObject.getString("titOriLiv");
+                sinopLiv = bookJSONObject.getString("sinopLiv");
+               // imgLiv = imgLiv.get("product_type");
+                numPagLiv = bookJSONObject.getString("numPagLiv");
+                anoLiv = bookJSONObject.getString("anoLiv");
+                numEdicaoLiv = bookJSONObject.getString("numEdicaoLiv");
             } catch (JSONException e){
                 e.printStackTrace();
             }
 
             JSONObject productJSONObject = new JSONObject();
             try{
-                productJSONObject.put("name", nameProduct);
-                productJSONObject.put("price", priceProduct);
-                productJSONObject.put("description", descriptionProduct);
-                productJSONObject.put("brand", brandProduct);
-                productJSONObject.put("product_type", typeProduct);
+                productJSONObject.put("isbn", ISBN);
+                productJSONObject.put("titLiv", titLiv);
+                productJSONObject.put("titOriLiv", titOriLiv);
+                productJSONObject.put("sinopLiv", sinopLiv);
+                productJSONObject.put("numPagLiv", numPagLiv);
+                productJSONObject.put("anoLiv", anoLiv);
+                productJSONObject.put("numEdicaoLiv", numEdicaoLiv);
 
-                makeupJSONString = makeupJSONObject.toString();
+                BookJSONString = bookJSONObject.toString();
             } catch (JSONException e){
                 e.printStackTrace();
             }
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        Log.d(LOG_TAG, makeupJSONString);
-        return makeupJSONString;
+        Log.d(LOG_TAG, BookJSONString);
+        return BookJSONString;
+    }
+
+    static String searchCli(String queryCli){
+        String cliJSONString = null;
+
+        try{
+            cliJSONString = searchCli(queryCli);
+            if(cliJSONString == null){
+                return null;
+            }
+
+            JSONObject cliJSONObject = new JSONObject(cliJSONString);
+
+            String IdCli = null;
+            String nomCli = null;
+            String titOriLiv = null;
+            String sinopLiv = null;
+            String imgLiv = null;
+            String numPagLiv = null;
+            String anoLiv = null;
+            String numEdicaoLiv = null;
+
+            try{
+                ISBN = bookJSONObject.getString("isbn");
+                titLiv = bookJSONObject.getString("titLiv");
+                titOriLiv = bookJSONObject.getString("titOriLiv");
+                sinopLiv = bookJSONObject.getString("sinopLiv");
+                // imgLiv = imgLiv.get("product_type");
+                numPagLiv = bookJSONObject.getString("numPagLiv");
+                anoLiv = bookJSONObject.getString("anoLiv");
+                numEdicaoLiv = bookJSONObject.getString("numEdicaoLiv");
+            } catch (JSONException e){
+                e.printStackTrace();
+            }
+
+            JSONObject productJSONObject = new JSONObject();
+            try{
+                productJSONObject.put("isbn", ISBN);
+                productJSONObject.put("titLiv", titLiv);
+                productJSONObject.put("titOriLiv", titOriLiv);
+                productJSONObject.put("sinopLiv", sinopLiv);
+                productJSONObject.put("numPagLiv", numPagLiv);
+                productJSONObject.put("anoLiv", anoLiv);
+                productJSONObject.put("numEdicaoLiv", numEdicaoLiv);
+
+                BookJSONString = bookJSONObject.toString();
+            } catch (JSONException e){
+                e.printStackTrace();
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Log.d(LOG_TAG, BookJSONString);
+        return BookJSONString;
     }
 }
